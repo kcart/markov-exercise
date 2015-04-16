a74750af1b1dcd140e1221007ec67faa082803c7
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 12:53:35 -0700

    Fixed part 1 tuple list of value assignment for dict

--- make-text-markov.py
+++ make-text-markov.py
@@ -1,6 +1,8 @@
 from sys import argv
 
 script, corpus = argv 
+print script
+print corpus
 
 def make_chains(corpus):
     open_file = open(corpus)
@@ -8,15 +10,23 @@ def make_chains(corpus):
     string_text = we_read_text.split()
     bigram_dict = {}
 
-    for i in range(len(string_text)-2):    
-        bigram_dict[(string_text[i], string_text[i+1])] = [string_text[i+2]]
+    for i in range(len(string_text)-2):
+        key = (string_text[i], string_text[i+1]) #assign stings to tuples, make tuples the keys
+        value = string_text[i+2] #value to be string following the tuple
+        if key not in bigram_dict: #assign key and value 
+            bigram_dict[key] = [value]
+        else:
+            bigram_dict[key].append(value) 
+            
+            """if key is already there, add value 
+            to existing list of values"""
 
     return bigram_dict     
 
 new_dictionary = make_chains(corpus) 
 
-def make_text(bigram_dict):
-   
+def make_text(chains):
+    print chains
     """Takes dictionary of markov chains; returns random text."""
   
  # return "Here's some random text."
