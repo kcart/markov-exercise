8c9cef9935169e8f76310c0a5320d6bd5e2a4e86
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 17:42:10 -0700

    done!

--- make-text-markov.py
+++ make-text-markov.py
@@ -1,9 +1,10 @@
+"""Our final working markov text generator. Code reviewed"""
+
 from sys import argv
 from random import choice
 
 script, corpus = argv 
-print script
-print corpus
+
 
 def make_chains(corpus):
     open_file = open(corpus)
@@ -24,21 +25,28 @@ def make_chains(corpus):
 
     return bigram_dict     
 
-new_dictionary = make_chains(corpus) 
+
 
 def make_text(chains):
+    word_list =[]
     rand_tuple = choice(chains.keys()) #picks a random key from dictionary, give it name rand_tuple
-    rand_word = choice(rand_tuple) #picks random word from tuple
-    assoc_value = choice(chains[rand_tuple]) #value associated with key rand_tuple
-    print chains.get((rand_word, assoc_value), "doesn't exist")
-   
+    for rand_tuple in chains: #for that random key in dictionary
+        rand_word = choice(rand_tuple) #picks random word from tuple
+        assoc_value = choice(chains[rand_tuple]) #value associated with key rand_tuple
+        if (rand_word, assoc_value) in chains: 
+            word_list.extend([rand_word, assoc_value])
+    return " ".join(word_list)
+       
 
 
-
-
-    
     """Takes dictionary of markov chains; returns random text."""
   
+
  # return "Here's some random text."
- 
-make_text(new_dictionary)
+bigram_dict = make_chains(corpus)  
+
+#print bigram_dict
+
+random_text = make_text(bigram_dict)
+
+print random_text
